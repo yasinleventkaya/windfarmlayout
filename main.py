0e238934a7a7ffae8487dc3d3caf419dca54b385
barankaya <barankayametu@gmail.com>
2021-02-28 16:43:07 +0300

    baran 333

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('baran 2')
+    print_hi('baran 333')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
